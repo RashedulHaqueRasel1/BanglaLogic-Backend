ad33738ddf622b3929974f3fec7f9d8c70c62fca
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-01-06 20:41:46 +0600

    MongoDB Connect & API Blogs Create

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 require('dotenv').config()
- 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 
 // middleWare....
@@ -12,6 +13,60 @@ app.use(cors({
     credentials: true
 }))
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6r4rx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const blogsCollection = client.db('BanglaLogic').collection('Blogs')
+
+
+        // Blogs Read & Show Data Clint Side .
+        app.get('/blogs', async (req, res) => {
+            const cursor = blogsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
+        // Push Data MongoDB
+        // app.post('/blogs', async (req, res) => {
+        //     const blogs = req.body;
+        //     blogs.cost= parseInt(blogs.cost)
+        //     // console.log(blogs)
+        //     const result = await blogsCollection.insertOne(blogs);
+        //     res.send(result)
+        // })
+
+
+
+
+         // Delete Single Data ..
+        // app.delete('/blogs/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await blogsCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+
+
+        console.log("Pinged your deployment. Bangla Logic successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 
